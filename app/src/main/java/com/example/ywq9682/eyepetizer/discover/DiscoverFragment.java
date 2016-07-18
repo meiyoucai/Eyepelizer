8e035dbd1a48554eef4f406a9f9cbb193f24ecbb
朱福林 <821487219@qq.com>
2016-07-18 17:26:18 +0800

    .猪

--- app/src/main/java/com/example/ywq9682/eyepetizer/discover/DiscoverFragment.java
+++ app/src/main/java/com/example/ywq9682/eyepetizer/discover/DiscoverFragment.java
@@ -1,20 +1,33 @@
 package com.example.ywq9682.eyepetizer.discover;
 
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.example.ywq9682.eyepetizer.R;
 import com.example.ywq9682.eyepetizer.base.BaseFragment;
 
 /**
  * Created by YWQ9682 on 2016/7/16.
  */
 public class DiscoverFragment extends BaseFragment {
+    private RecyclerView recyclerView;
+
+
     @Override
     public int setLayout() {
-        return 0;
+        return R.layout.fragment_discover;
     }
 
     @Override
     public void initView(View view) {
+
+        initHead();
+
+
+    }
+
+    private void initHead() {
+        
 
     }
 
