7d93e540ceaa8d96bdb709b4e8b24f9fb3557edc
杨文庆 <ywq9682@163.com>
2016-07-19 17:19:06 +0800

    no message

--- app/src/main/java/com/example/ywq9682/eyepetizer/video/VideoActivity.java
+++ app/src/main/java/com/example/ywq9682/eyepetizer/video/VideoActivity.java
@@ -5,14 +5,12 @@ import android.content.Intent;
 import com.example.ywq9682.eyepetizer.R;
 import com.example.ywq9682.eyepetizer.base.BaseActivity;
 
-import fm.jiecao.jcvideoplayer_lib.JCVideoPlayer;
-import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerStandard;
 
 /**
  * Created by YWQ9682 on 2016/7/19.
  */
 public class VideoActivity extends BaseActivity {
-    private JCVideoPlayerStandard jcVideoPlayerStandard;
+
     @Override
     public int setLayout() {
         return R.layout.activity_video;
@@ -22,8 +20,6 @@ public class VideoActivity extends BaseActivity {
     public void initView() {
         Intent intent = getIntent();
         String url = intent.getStringExtra("url");
-        jcVideoPlayerStandard = (JCVideoPlayerStandard) findViewById(R.id.custom_videoplayer_standard);
-        jcVideoPlayerStandard.setUp(url, "呵呵");
 
     }
 
@@ -32,9 +28,5 @@ public class VideoActivity extends BaseActivity {
 
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        JCVideoPlayer.releaseAllVideos();
-    }
+
 }
