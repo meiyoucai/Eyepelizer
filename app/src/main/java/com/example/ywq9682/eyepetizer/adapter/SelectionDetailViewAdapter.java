f60b2a7754b9356ca24bdd7405964b196d745dcd
杨文庆 <ywq9682@163.com>
2016-07-30 10:03:44 +0800

    no message

--- app/src/main/java/com/example/ywq9682/eyepetizer/adapter/SelectionDetailViewAdapter.java
+++ app/src/main/java/com/example/ywq9682/eyepetizer/adapter/SelectionDetailViewAdapter.java
@@ -3,33 +3,32 @@ package com.example.ywq9682.eyepetizer.adapter;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.content.Context;
-<<<<<<< HEAD
+
 import android.content.Intent;
 import android.graphics.Color;
-=======
->>>>>>> ae042ec5be2a3f86c3f97a23c5be8d142e3adef6
+
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-<<<<<<< HEAD
+
 import android.view.animation.AlphaAnimation;
-=======
+
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
->>>>>>> ae042ec5be2a3f86c3f97a23c5be8d142e3adef6
+
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.ywq9682.eyepetizer.R;
 import com.example.ywq9682.eyepetizer.bean.SelectionListBean;
-<<<<<<< HEAD
+
 import com.example.ywq9682.eyepetizer.selection.FlyTextView;
 import com.example.ywq9682.eyepetizer.video.VideoPlayerActivity;
-=======
->>>>>>> ae042ec5be2a3f86c3f97a23c5be8d142e3adef6
+
 
 import java.util.ArrayList;
 
@@ -37,14 +36,14 @@ import java.util.ArrayList;
  * Created by dllo on 16/7/21.
  */
 public class SelectionDetailViewAdapter extends PagerAdapter {
-<<<<<<< HEAD
+
 
     private Context context;
     private ArrayList<SelectionListBean> selectionListBean;
     private int pos,positions;
-=======
+
     private int pos;
->>>>>>> ae042ec5be2a3f86c3f97a23c5be8d142e3adef6
+
 
     public void setPos(int pos) {
         this.pos = pos;
@@ -70,7 +69,7 @@ public class SelectionDetailViewAdapter extends PagerAdapter {
     }
 
     @Override
-<<<<<<< HEAD
+
     public Object instantiateItem(ViewGroup container, final int position) {
         positions=position+pos;
         View view = LayoutInflater.from(context).inflate(R.layout.item_selection_detail_view, container, false);
@@ -81,7 +80,7 @@ public class SelectionDetailViewAdapter extends PagerAdapter {
         TextView selectionCategory = (TextView) view.findViewById(R.id.selection_category);
         TextView selectionDuration = (TextView) view.findViewById(R.id.selection_duration);
         FlyTextView selectionDescription = (FlyTextView) view.findViewById(R.id.selection_description);
-        TextView selectionCollectionCountTv = (TextView) view.findViewById(R.id.selection_collectionCount_tv);
+        TextView selectionCollectionCountTv = (TextView) view.findViewById(R.id.selection_collectionCount);
         TextView selectionShareCountTv = (TextView) view.findViewById(R.id.selection_shareCount_tv);
         TextView selectionReplyCountTv = (TextView) view.findViewById(R.id.selection_reply_count_tv);
         selectionPlay.setOnClickListener(new View.OnClickListener() {
@@ -121,14 +120,14 @@ public class SelectionDetailViewAdapter extends PagerAdapter {
 
 
         ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(selectionDetailIv, "scaleX", 1, 1.2f);
-=======
+
     public Object instantiateItem(ViewGroup container, int position) {
         pos=position;
         View view = LayoutInflater.from(context).inflate(R.layout.item_selection_detail_view, container, false);
         ImageView selectionDetailIv = (ImageView) view.findViewById(R.id.selection_detail_iv);
         Glide.with(context).load(selectionListBean.get(position).getImageUrl()).into(selectionDetailIv);
         ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(selectionDetailIv, "scaleX", 1, 1.1f);
->>>>>>> ae042ec5be2a3f86c3f97a23c5be8d142e3adef6
+
         objectAnimator.setRepeatMode(ValueAnimator.REVERSE);
         objectAnimator.setRepeatCount(ValueAnimator.INFINITE);
         ObjectAnimator animator = ObjectAnimator.ofFloat(selectionDetailIv, "scaleY", 1, 1.1f);
