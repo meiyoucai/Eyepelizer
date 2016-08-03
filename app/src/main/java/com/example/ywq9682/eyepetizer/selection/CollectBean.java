ddc587d942f04e451786df0dfb06a712c2b3bc27
程传旨 <296731949@qq.com>
2016-08-03 14:03:27 +0800

    完成

---
+++ app/src/main/java/com/example/ywq9682/eyepetizer/selection/CollectBean.java
@@ -0,0 +1,7 @@
+package com.example.ywq9682.eyepetizer.selection;
+
+/**
+ * Created by dllo on 16/8/3.
+ */
+public class CollectBean {
+}
---
+++ app/src/main/java/com/example/ywq9682/eyepetizer/tools/Window.java
@@ -0,0 +1,7 @@
+package com.example.ywq9682.eyepetizer.tools;
+
+/**
+ * Created by dllo on 16/7/30.
+ */
+public class Window {
+}
